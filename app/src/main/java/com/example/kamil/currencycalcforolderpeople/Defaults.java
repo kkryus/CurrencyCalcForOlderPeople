4a80e04a8fd694e133e0ee3fee37de43f053ea32
kkryus <kamilkryus@gmail.com>
2018-03-20 19:19:50 +0100

    updating currencies
    now app downloads currencies from url and saves it to file

--- app/src/main/java/com/example/kamil/currencycalcforolderpeople/Defaults.java
+++ app/src/main/java/com/example/kamil/currencycalcforolderpeople/Defaults.java
@@ -3,6 +3,7 @@ package com.example.kamil.currencycalcforolderpeople;
 public class Defaults {
     public static int imageButtonHeight = (int) (115 * Settings.multplySize);
     public static int textViewTextSize = (int) (34 * Settings.multplySize);
+    public static int alertTextSize = (int) (20 * Settings.multplySize);
     public static int shortcutViewTextSize = (int) (30 * Settings.multplySize);
     public static int buttonTextSize = (int) (14 * Settings.multplySize);
 }
